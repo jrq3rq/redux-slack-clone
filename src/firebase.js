f2c535c7eeb032416e8c2af67fbfc17b44fff1b2
YOUR NAME <YOUR EMAIL ADDRESS>
2021-02-20 15:15:31 -0800

    added header

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC3ZTAIRmRwEXstoaHu69CmsIovzgs8sI0",
+  authDomain: "netflix-redux-clone.firebaseapp.com",
+  projectId: "netflix-redux-clone",
+  storageBucket: "netflix-redux-clone.appspot.com",
+  messagingSenderId: "19718861135",
+  appId: "1:19718861135:web:7b1c13b475c8a08582553a",
+  measurementId: "G-P9ENMF8ZSW",
+};
